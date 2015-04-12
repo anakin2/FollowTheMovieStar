840bfbbc5fd50f998a181f6fe9a8724f5cbeb7d2
ggm <godgunman@gmail.com>
2015-04-12 01:45:30 +0000

    add Parse lib. add EditText and Button

--- app/src/main/java/tw/ggmlab/followthemoviestar/MainActivity.java
+++ app/src/main/java/tw/ggmlab/followthemoviestar/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -20,6 +21,10 @@ public class MainActivity extends ActionBarActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    public void clickSearch(View view) {
+
     }
 
     @Override
